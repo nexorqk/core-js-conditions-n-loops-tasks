7dc00742be5cb4d744eb6bc146a1b233daf7d860
nexorqk <pkms2a10@gmail.com>
2024-12-10 18:57:04 +0300

    feat: add 11 tests passing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -195,50 +195,48 @@ function convertNumberToString(numberStr) {
   for (let i = 0; i < len; i += 1) {
     switch (numberStr[i]) {
       case '0':
-        res += 'zero ';
+        res += 'zero';
         break;
       case '1':
-        res += 'one ';
+        res += 'one';
         break;
       case '2':
-        res += 'two ';
+        res += 'two';
         break;
       case '3':
-        res += 'three ';
+        res += 'three';
         break;
       case '4':
-        res += 'four ';
+        res += 'four';
         break;
       case '5':
-        res += 'five ';
+        res += 'five';
         break;
       case '6':
-        res += 'six ';
+        res += 'six';
         break;
       case '7':
-        res += 'seven ';
+        res += 'seven';
         break;
       case '8':
-        res += 'eight ';
+        res += 'eight';
         break;
       case '9':
-        res += 'nine ';
+        res += 'nine';
         break;
       case '-':
-        res += 'minus ';
+        res += 'minus';
         break;
       default:
-        res += 'point ';
+        res += 'point';
+    }
+
+    if (i !== len - 1) {
+      res += ' ';
     }
   }
 
-  let strWOLastSpace = '';
-
-  for (let i = 0; i < res.length - 1; i += 1) {
-    strWOLastSpace += res[i];
-  }
-
-  return strWOLastSpace;
+  return res;
 }
 
 /**
@@ -253,8 +251,16 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const lenHalf = Math.floor(str.length / 2);
+
+  for (let i = 0; i < lenHalf; i += 1) {
+    if (str[i] !== str[str.length - i - 1]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /**
@@ -271,8 +277,14 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      return i;
+    }
+  }
+
+  return -1;
 }
 
 /**
@@ -290,8 +302,17 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let whileNum = num;
+
+  while (whileNum > 0) {
+    if (whileNum % 10 === digit) {
+      return true;
+    }
+    whileNum = Math.floor(whileNum / 10);
+  }
+
+  return false;
 }
 
 /**
@@ -307,8 +328,26 @@ function isContainNumber(/* num, digit */) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  for (let i = 2; i < arr.length; i += 1) {
+    let leftSum = 0;
+    let rightSum = 0;
+
+    for (let j = 0; j < arr.length; j += 1) {
+      if (j < i) {
+        leftSum += arr[j];
+      }
+      if (j > i) {
+        rightSum += arr[j];
+      }
+    }
+
+    if (leftSum === rightSum) {
+      return i;
+    }
+  }
+
+  return -1;
 }
 
 /**
